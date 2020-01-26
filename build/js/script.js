34171c72ff3ed2b7c43a3cb52dcafd3984be36cc
Taisiia <taisiia.pomazan@gmail.com>
2020-01-26 19:48:41 +0300

    ms

--- build/js/script.js
+++ build/js/script.js
@@ -67,65 +67,6 @@ btnRight.onclick = function() {
   }
   slider();
 };
-
-//--------------------------pagination
-
-const itemsArr = [...document.querySelectorAll('menu-main__item')];
-const parent = document.querySelector('.menu-main__restaurant');
-const paginationElem = document.querySelector('#pagination').textContent;
-console.log(paginationElem);
-
-let current_page = 1;
-let columns = 3;
-
-function DisplayList (items, columns_per_page, page) {
-	// wrapper.innerHTML = "";
-	page--;
-
-	let start = columns_per_page * page;
-	let end = start + columns_per_page;
-	let paginatedItems = items.slice(start, end);
-
-	// for (let i = 0; i < paginatedItems.length; i++) {
-	// 	let item = paginatedItems[i];
-		// let item_element = document.createElement('div');
-		// item_element.classList.add('item');
-		// item_element.innerText = item;
-		// wrapper.appendChild(item_element);
-	// }
-}
-
-function SetupPagination (items, wrapper, columns_per_page) {
-	wrapper.innerHTML = "";
-
-	let page_count = Math.ceil(items.length / columns_per_page);
-	for (let i = 1; i < page_count + 1; i++) {
-		let btn = PaginationButton(i, items);
-		wrapper.appendChild(btn);
-	}
-}
-
-function PaginationButton (page, items) {
-	let button = document.createElement('button');
-	button.innerText = page;
-
-	if (current_page == page) button.classList.add('active');
-
-	button.addEventListener('click', function () {
-		current_page = page;
-		DisplayList(items, parent, columns, current_page);
-
-		let current_btn = document.querySelector('.pagenumbers button.active');
-		current_btn.classList.remove('active');
-
-		button.classList.add('active');
-	});
-
-	return button;
-}
-
-DisplayList(itemsArr, columns, current_page);
-SetupPagination(itemsArr, paginationElem, columns);
 function search() {
   let input = document.querySelector('.menu-main__search-form');
   let filterValue = input.value.toUpperCase();
